d38b12af46e20528d8087af785aeed42e9029e5c
Thomas Gamble <thomas_gamble@homedepot.com>
2016-03-11 20:23:41 -0500

    added tests and functionality for saving and retreiving

--- server.js
+++ server.js
@@ -1,62 +1,73 @@
-var express  = require('express');
-var app        = express();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 var Book = require('./app/models/book');
 
 
-var uri = 'localhost/db_name';
-if(process.env.VCAP_SERVICES)
-{
-    var vcap_services = JSON.parse(process.env.VCAP_SERVICES)
-    uri = vcap_services.mongo[0].credentials.uri
+
+
+if(process.env.VCAP_SERVICES){
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var mongoUri = env['p-mongodb'][0]['credentials']['uri'];
 }
-console.log(uri);
-mongoose.connect('mongodb://' + uri);
-//mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+else{
+    var mongoUri = 'mongodb://localhost/db_name';
+}
 
+console.log(mongoUri);
+mongoose.connect(mongoUri);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
-router.use(function(req, res, next) {
-    console.log(req.body);
+router.use(function (req, res, next) {
     next();
 });
 
-router.get('/', function(req, res) {
-    res.json({ message: 'Move along, nothing to see here' });
+router.get('/', function (req, res) {
+    res.json({message: 'Move along, nothing to see here'});
 });
 
 router.route('/books')
-
-    .post(function(req, res) {
-
+    .post(function (req, res) {
         var book = new Book();
-        book.case = req.body.case;
 
-        book.save(function(err) {
-            if (err)
+        book.case = JSON.stringify(req.body);
+        book.save(function (err) {
+            if (err) {
                 res.send(err);
-
-            res.json({ message: 'Book list saved!' });
+            }
+            res.json({message: 'Book list saved!'});
         });
     })
-
-    .get(function(req, res) {
-    Book.find(function(err, books) {
-        if (err)
-            res.send(err);
-
-        res.json(books);
+    .get(function (req, res) {
+        Book.find(function (err, books) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(books);
+        });
     });
-});
+
+
+router.route('/books/newest')
+    .get(function (req, res) {
+        Book.findOne().sort({_id: -1}).exec(function (err, book) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(book.case);
+        });
+    });
 
 app.use('/api', router);
 
 app.listen(port);
 console.log('Express running on ' + port);
+
+module.exports = app;
---
+++ test/test-server.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Books', function() {
+    it('should list ALL booklists on /api/books GET', function(done) {
+        chai.request(server)
+            .get('/api/books')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+    it('should add a SINGLE booklist on /books POST', function(done) {
+        chai.request(server)
+            .post('/api/books')
+            .send({'Author': 'Joe', 'Title': 'Joe\'s book'})
+            .end(function(err, res){
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('message');
+                res.body.message.should.equal('Book list saved!');
+                done();
+            });
+    });
+    it('should return the newest booklist on /api/books/newest GET', function(done) {
+        chai.request(server)
+            .get('/api/books/newest')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
