f26fe2cc44b1d1b1355233b1b0906af692b5823f
Thomas Gamble <thomas_gamble@homedepot.com>
2016-03-29 22:23:31 -0400

    wip - mysql conversion

--- seed.sql
+++ seed.sql
@@ -1,20 +1,22 @@
 #!/bin/bash
   
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 E_BADARGS=65
 MYSQL=`which mysql`
   
 Q1="CREATE DATABASE IF NOT EXISTS $1;"
-Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$3';"
-Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
-Q4="FLUSH PRIVILEGES;"
-SQL="${Q1}${Q2}${Q3}${Q4}"
+Q2="GRANT USAGE ON *.* TO $3@localhost IDENTIFIED BY '$4';"
+Q3="GRANT ALL PRIVILEGES ON $1.* TO $3@localhost;"
+Q4="USE $1;"
+Q5="CREATE TABLE IF NOT EXISTS $2 (id int(5) NOT NULL AUTO_INCREMENT, json BLOB NOT NULL, PRIMARY KEY (id));"
+Q6="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
   
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 dbname dbuser dbpass"
+  echo "Usage: $0 dbname dbtable dbuser dbpass"
   exit $E_BADARGS
 fi
   
-$MYSQL --user=$2 --password=$3 -e "$SQL"
+$MYSQL --user=$3 --password=$4 -e "$SQL"
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var mongoose    = require('mongoose');
-var Book        = require('./app/models/book');
-var mysql       = require('mysql');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Book = require('./app/models/book');
+var mysql = require('mysql');
 
 var port = process.env.PORT || 3000;
 var router = express.Router();
@@ -11,39 +11,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-if(process.env.VCAP_SERVICES){
+if (process.env.VCAP_SERVICES) {
     var env = JSON.parse(process.env.VCAP_SERVICES);
     //var mongoUri = env['p-mongodb'][0]['credentials']['uri'];
     var mongoUri = env['mongolab'][0]['credentials']['uri'];
 }
-else{
+else {
     var mongoUri = 'mongodb://localhost/db_name';
 }
 mongoose.connect(mongoUri);
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'bookcase'
+var pool  = mysql.createPool({
+    connectionLimit : 5,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bookcase'
 });
 
-connection.connect(function(err){
-    if(!err) {
-        console.log("Database is connected ... \n\n");
-    } else {
-        console.log("Error connecting database ... \n\n");
-    }
-});
-
-connection.query('SELECT * from bookcase LIMIT 1', function(err, rows, fields) {
-    connection.end();
-    if (!err)
-        console.log('The solution is: ', rows);
-    else
-        console.log('Error while performing Query.');
-});
-
+//var connection = mysql.createConnection('mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
 
 router.use(function (req, res, next) {
     next();
@@ -64,33 +50,58 @@ router.route('/books')
             }
             res.json({message: 'Book list saved!'});
         });
+
+        pool.getConnection(function(err, connection) {
+            // Use the connection
+            connection.query('INSERT INTO list SET json ?', req.body, function (err, results) {
+                connection.release();
+                if (!err)
+                    console.log('Inserted bookcase: ', results.id);
+                else
+                    console.log('Error while inserting: ', req.body, err);
+            });
+        });
+
+
     })
     .get(function (req, res) {
+        
         Book.find(function (err, books) {
             if (err) {
                 res.send(err);
             }
             res.json(books);
         });
+        pool.getConnection(function(err, connection) {
+            // Use the connection
+            connection.query('SELECT json FROM list order by id desc limit 1', function (err, results) {
+                connection.release();
+                if (!err)
+                    console.log('Selected: ', results);
+                else
+                    console.log('Selected ', results, err);
+            });
+        });
+
     });
 
 router.route('/book/:book_id')
 
-    .get(function(req, res) {
-        Book.findById(req.params.book_id, function(err, book) {
+    .get(function (req, res) {
+        Book.findById(req.params.book_id, function (err, book) {
             if (err)
                 res.send(err);
             res.json(book);
         });
     })
 
-    .delete(function(req, res) {
+    .delete(function (req, res) {
         Book.remove({
             _id: req.params.book_id
-        }, function(err, book) {
+        }, function (err, book) {
             if (err)
                 res.send(err);
-            res.json({ message: 'Successfully deleted' });
+            res.json({message: 'Successfully deleted'});
         });
     });
 
